85106dd26dd2cdc6085a4d9124e23a9059020b3f
lainys <is-kirillka@yandex.ru>
2018-04-12 11:49:22 +0300

    prepare files

--- src/main/java/ru/miet/orgact/Main.java
+++ src/main/java/ru/miet/orgact/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/layouts/main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
 }
