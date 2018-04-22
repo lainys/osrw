8069848268bf88f6582a19d9d48b040ac3b36772
lainys <is-kirillka@yandex.ru>
2018-04-22 13:18:53 +0300

    начал делать 3 панель

--- src/main/java/ru/miet/orgact/controllers/ThirdTabController.java
+++ src/main/java/ru/miet/orgact/controllers/ThirdTabController.java
@@ -1,4 +1,42 @@
 package ru.miet.orgact.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+
 public class ThirdTabController {
+
+    @FXML
+    private AnchorPane placePanel;
+
+
+    public void initialize() {
+
+    }
+
+    @FXML
+    public void selectPlace(ActionEvent event) {
+        Node source = getLayout(((RadioButton) event.getSource()).getId());
+        placePanel.getChildren().clear();
+        placePanel.getChildren().add(source);
+    }
+
+    public Node getLayout(String name) {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("/layouts/" + name + ".fxml"));
+            return root;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
